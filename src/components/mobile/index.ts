6bebb216d0b0109773caa50e3e843333a7211a31
Francisco <franciscoaneto13@gmail.com>
2026-07-09 01:24:10 -0300

    chore(ui): add barrel export for mobile components

---
+++ src/components/mobile/index.ts
@@ -0,0 +1,4 @@
+export { MobileBottomNav } from "./MobileBottomNav";
+export { StickyBottomCTA } from "./StickyBottomCTA";
+export { TouchCarousel } from "./TouchCarousel";
+export { CollapsibleCard } from "./CollapsibleCard";
